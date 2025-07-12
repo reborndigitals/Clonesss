39bd46910cb217a31cafd6ae65d0756d82c828d2
reborndigitals <reborndigitals@gmail.com>
2025-07-12 20:34:21 +0530

    play.py

--- BABYMUSIC/utils/inline/play.py
+++ BABYMUSIC/utils/inline/play.py
@@ -32,26 +32,22 @@ def stream_markup_timer(_, vidid, chat_id, played, dur):
     duration_sec = time_to_seconds(dur)
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
-    if 0 < umm <= 10:
-        bar = "◉—————————"
+    if 0 < umm <= 40:
+        bar = "✪⭑⭑⭑⭑⭑⭑⭑⭑⭑"
     elif 10 < umm < 20:
-        bar = "—◉————————"
-    elif 20 <= umm < 30:
-        bar = "——◉———————"
+        bar = "⭑✪⭑⭑⭑⭑⭑⭑⭑⭑⭑"
+    elif 20 < umm < 30:
+        bar = "⭑⭑✪⭑⭑⭑⭑⭑⭑⭑⭑⭑"
     elif 30 <= umm < 40:
-        bar = "———◉——————"
+        bar = "⭑⭑⭑✪⭑⭑⭑⭑⭑⭑⭑"
     elif 40 <= umm < 50:
-        bar = "————◉—————"
+        bar = "⭑⭑⭑⭑✪⭑⭑⭑⭑⭑⭑"
     elif 50 <= umm < 60:
-        bar = "—————◉————"
-    elif 60 <= umm < 70:
-        bar = "——————◉———"
-    elif 70 <= umm < 80:
-        bar = "———————◉——"
-    elif 80 <= umm < 95:
-        bar = "————————◉—"
+        bar = "⭑⭑⭑⭑⭑⭑✪⭑⭑⭑⭑"
+    elif 50 <= umm < 70:
+        bar = "⭑⭑⭑⭑⭑⭑⭑✪⭑⭑⭑"
     else:
-        bar = "—————————◉"
+        bar = "⭑⭑⭑⭑⭑⭑⭑⭑⭑⭑✪"
     buttons = [
          [
             InlineKeyboardButton(
@@ -68,10 +64,10 @@ def stream_markup_timer(_, vidid, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(
-                text="ᴏᴡɴᴇʀ", url=f"https://t.me/untold_coder4"
+                text="HeartBeat_Offi", url=f"https://t.me/untold_coder4"
             ),
             InlineKeyboardButton(
-                text="ᴄʜᴀᴛ", url=f"https://t.me/untold_coder"
+                text="HeartBeat_Fam", url=f"https://t.me/untold_coder"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
