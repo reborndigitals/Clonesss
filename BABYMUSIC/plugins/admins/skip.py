0e38ccfe349a22f2d7223c338581fa7115e11a52
reborndigitals <reborndigitals@gmail.com>
2025-07-17 01:33:34 +0530

    skip.py

--- BABYMUSIC/plugins/admins/skip.py
+++ BABYMUSIC/plugins/admins/skip.py
@@ -9,7 +9,7 @@ from BABYMUSIC.utils.database import get_loop
 from BABYMUSIC.utils.decorators import AdminRightsCheck
 from BABYMUSIC.utils.inline import close_markup, stream_markup, stream_markup2
 from BABYMUSIC.utils.stream.autoclear import auto_clean
-from BABYMUSIC.utils.thumbnails import get_thumb
+from BABYMUSIC.utils.thumbnails import gen_thumb
 from config import BANNED_USERS
 
 
