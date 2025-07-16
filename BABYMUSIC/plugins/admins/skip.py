1a44528c8be21ac242ed29b2bba7845c8616d40c
reborndigitals <reborndigitals@gmail.com>
2025-07-17 01:40:24 +0530

    skip.py

--- BABYMUSIC/plugins/admins/skip.py
+++ BABYMUSIC/plugins/admins/skip.py
@@ -9,7 +9,7 @@ from BABYMUSIC.utils.database import get_loop
 from BABYMUSIC.utils.decorators import AdminRightsCheck
 from BABYMUSIC.utils.inline import close_markup, stream_markup, stream_markup2
 from BABYMUSIC.utils.stream.autoclear import auto_clean
-from BABYMUSIC.utils.thumbnails import gen_thumb
+from BABYMUSIC.utils.thumbnails import get_thumb
 from config import BANNED_USERS
 
 
@@ -119,7 +119,7 @@ async def skip(cli, message: Message, _, chat_id):
         except:
             return await message.reply_text(_["call_6"])
         button = stream_markup2(_, chat_id)
-        img = await gen_thumb(videoid)
+        img = await get_thumb(videoid)
         run = await message.reply_photo(
             photo=img,
             caption=_["stream_1"].format(
@@ -152,7 +152,7 @@ async def skip(cli, message: Message, _, chat_id):
         except:
             return await mystic.edit_text(_["call_6"])
         button = stream_markup(_, videoid, chat_id)
-        img = await gen_thumb(videoid)
+        img = await get_thumb(videoid)
         run = await message.reply_photo(
             photo=img,
             caption=_["stream_1"].format(
@@ -225,7 +225,7 @@ async def skip(cli, message: Message, _, chat_id):
             db[chat_id][0]["markup"] = "tg"
         else:
             button = stream_markup(_, videoid, chat_id)
-            img = await gen_thumb(videoid)
+            img = await get_thumb(videoid)
             run = await message.reply_photo(
                 photo=img,
                 caption=_["stream_1"].format(
