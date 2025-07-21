c1a0c41f5613befd295c09e660e971457b746d4c
reborndigitals <reborndigitals@gmail.com>
2025-07-21 09:47:41 +0530

    start.py

--- BABYMUSIC/cplugin/start.py
+++ BABYMUSIC/cplugin/start.py
@@ -107,7 +107,7 @@ async def start_pm(client, message: Message, _):
             )
         ],
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=OWNER_ID),
+            InlineKeyboardButton(text=_["S_B_7"], url=config.CHAT),
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
         ],
         [
