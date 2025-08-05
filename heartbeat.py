9a8635fa0b145c76effd6c7a666e1a1bb6c783fa
reborndigitals <reborndigitals@gmail.com>
2025-08-05 16:03:46 +0530

    heartbeat.py

---
+++ heartbeat.py
@@ -0,0 +1,18 @@
+# heartbeat.py
+
+from flask import Flask
+from threading import Thread
+
+app = Flask(name)
+
+@app.route('/')
+def home():
+    return "Bot is running on Render!"
+
+def run():
+    app.run(host='0.0.0.0', port=10000)
+
+def heartbeat():
+    t = Thread(target=run)
+    t.daemon = True
+    t.start()
